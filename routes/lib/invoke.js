5a4cfca69e5de5385244ec742ab617bde380f16c
catnap421 <wlq7410@naver.com>
2020-11-13 04:43:54 +0900

    [routes][1.1] Update registerDDo

--- routes/lib/invoke.js
+++ routes/lib/invoke.js
@@ -33,7 +33,7 @@ async function invoke(fcn, args) {
         if( fcn == "registerVC" )
             await contract.submitTransaction('registerVC', args.key, args.issuerDID, args.controller, args.issuanceDate, args.expirationDate, args.sig, args.sigType, args.hashType, args.claim);
         else if( fcn == "registerDDo" )
-            await contract.submitTransaction('registerDDo', args.key, args.pubkey, args.pubkeyType, args.context, args.sType, args.sEndpoint); 
+            await contract.submitTransaction('registerDDo', args.key, args.pubkey, args.pubkeyType, args.context, args.sID, args.sType, args.sEndpoint); 
         else if (fcn == "removeDDo") {
             await contract.submitTransaction('removeDDo', args);
             logger.info("Delete Success.");
