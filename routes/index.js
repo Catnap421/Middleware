419b44356c4abc820fbe662ac0a23212dd7c9d2a
catnap421 <wlq7410@naver.com>
2020-11-18 00:09:10 +0900

    [index.js][1.1]Upgrade verification

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 const logger = require(process.cwd()+'/config/winston');
-const network = require("fabric-network")
+const crypto = require('crypto');
+const fs = require('fs');
+const pdf = require('pdf-parse');
 
 router.get('/', function(req, res){
   console.log("START /");
@@ -12,14 +14,22 @@ router.get('/', function(req, res){
 router.post('/upload', multer({ dest: 'uploads/'}).single('file'), function(req, res){
   logger.info(`req.body is ${req.body.user}, ${req.body.domain}, ${req.body.apikey}`);
   logger.info(`req.file is ${req.file}`);
+  const dataBuffer = fs.readFileSync(`./uploads/${req.file.filename}`);
+
+  pdf(dataBuffer).then(function(data) {
+    console.log('data.text:');
+    var arr = data.text.split('\n');
+    var i = 0;
+    arr.forEach(el => console.log(`${++i}: ${el}`));
+  });
+  // 여기에서 did를 뽑아내야 함
+  let did = 'exampleCert57228'
   // 파일 읽기 -> 분해해서 질의
-  // const query = require("./lib/query");
-  // logger.info('GET /user/vc/:did');
-  // const path = req.path.split('/')[2];
-  // const ret = query("queryVC", req.query.user, req.query.domain, path, req.headers.apikey); // await가 안 먹히는 이유가 뭐지?
-  // if(!ret.hasOwnProperty('status')) res.status(200).json(ret.toString());
-  // else if(ret.status === 401) res.status(401).send('Unauthorized');
-  // else res.status(404).send('Error');
+  const query = require("./lib/query");
+
+
+  const ret = query("queryVC", req.body.user, req.body.domain, did, req.body.apikey); // await가 안 먹히는 이유가 뭐지?
+  console.log(ret)
   res.status(204).send();
 })
 
